4342243ae25596e92d8f4df59b83270e0db6ce47
theunmeshraj <unmeshraj.raj@gmail.com>
2025-10-24 17:32:55 +0530

    youtube changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -582,7 +582,7 @@ const LandingPage = () => {
         <div className="relative aspect-video overflow-hidden rounded-2xl border border-yellow-300/30 shadow-lg hover:shadow-[0_0_30px_rgba(255,215,0,0.25)] transition duration-300">
           <iframe
             className="absolute inset-0 w-full h-full rounded-2xl"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=LYveQmOBGIuofcHp"
+            src="https://www.youtube.com/embed/{}"
             title="CTF Event Guide"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
